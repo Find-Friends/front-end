c624a4e5cd1fccc34afb53370953c69165cbf758
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-21 14:59:10 -0400

    profile page coming along

--- friend-finder/src/components/Profile.js
+++ friend-finder/src/components/Profile.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-export default function Profile() {
+export default function ProfileForm() {
 
     const [search, setSearch] = useState([]); 
 
@@ -16,18 +16,75 @@ export default function Profile() {
 
     return (
 
-<section classname='FormContainer'>
+<div classname='FormContainer'>
 
     <Form>
+        {/* FIRST NAME */}
         <input className='input'
         type='text'
         placeholder='Enter First Name'
         name='firstname'
-        onChange={handleChange}
-        />
+        onChange={handleChange}/>
+          {touched.firstname && errors.firstname && (
+         <p className='error'>{errors.firstname}</p>
+        )}
+
+        {/* LAST NAME */}
+        <input className='input'
+        type='text'
+        placeholder='Enter Last Name'
+        name='lastname'/>
+          {touched.lastname && errors.lastname && (
+         <p className='error'>{errors.lastname}</p>
+        )}
+
+        {/* AGE */}  
+        <input className='input'
+        type='text'
+        placeholder='Enter Age'
+        name='age'/>
+          {touched.age && errors.age && (
+         <p className='error'>{errors.age}</p>
+        )}
+
+          {/* LOCATION */}  
+          <input className='input'
+        type='text'
+        placeholder='Enter Location'
+        name='location'/>
+          {touched.location && errors.location && (
+         <p className='error'>{errors.location}</p>
+        )}
+
     </Form>
-</section>
 
+</div>
     );
+};
 
-}
+const FormikProfile = withFormik({
+    mapPropsToValues({firstname, lastname, age, location}) {
+        return {
+            firstname: firstname || "",
+            lastname: lastname || "",
+            age: age || "",
+            location: location || ""
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        firstname: Yup.string().required('Please Enter First Name'),
+        lastname: Yup.string().required('Please Enter Last Name'),
+        age: Yup.string().required('Please Enter age'),
+        location: Yup.string().required('Please Enter location')
+
+    }),
+    handleSubmit(values, {setStatus}) {
+        axios.post('', values)
+        .then(res => {setStatus(res.data); })
+        .catch(err => console.log(err.response));
+    }
+
+})(ProfileForm);
+export default FormikProfile;
+console.log('formik profile', FormikProfile)
