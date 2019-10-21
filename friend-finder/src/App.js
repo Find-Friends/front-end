4c207a49e70d9c2b2fa26595cd463c9d01dd2af5
DannyManzietti <danmanzietti@gmail.com>
2019-10-21 18:23:33 -0400

    SignUp done and also privateRoute

--- friend-finder/src/App.js
+++ friend-finder/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Login from './components/Login';
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div >
+    <div>
       <h1>Friend Finder</h1>
-      <Route path='/login' component={Login} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
     </div>
   );
 }
