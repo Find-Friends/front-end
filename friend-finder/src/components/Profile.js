d46a7240b4b9ab9d40dbe7739246f89111df4aba
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-22 13:28:28 -0400

    added authorization

--- friend-finder/src/components/Profile.js
+++ friend-finder/src/components/Profile.js
@@ -1,30 +1,31 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 import ProfileCard from './ProfileCard';
 import ProfileForm from './ProfileForm';
+
+
 export default function Profile() {
 
     const [friends, setFriends] = useState([]);
 
-    // useEffect(()=> {
+    useEffect(()=> {
 
-    //     axios
-    //    .get('https://find-friends-api.herokuapp.com/api/users/:id/friends')
-    //     .then(response => {
-    //         setFriends(response.data); 
-    //         console.log(response)})
-    //     .catch(err => console.log(err.response));
+       axiosWithAuth()
+       .get('https://find-friends-api.herokuapp.com/api/users/:id/friends')
+        .then(response => {
+            setFriends(response.data); 
+            console.log(response)})
+        .catch(err => console.log(err.response));
 
 
-    // }, [])
+    }, [])
 
     return(
 
+        <section className="friend-list">
 
         
-
-        <section className="friend-list">
 
         <ProfileForm/>
         
@@ -34,7 +35,6 @@ export default function Profile() {
             return(
                 
                 <div>
-                    <a>Friend</a>
                
                 <ProfileCard key={friend.id} friend1={friend}/>
 
