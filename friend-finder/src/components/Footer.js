bb7b5f53d7d888547857c9cc8479e441517135a6
Rashmi <poddar.rashmi@gmail.com>
2019-10-24 16:19:14 -0400

    Added footer

---
+++ friend-finder/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+    <h6>&copy; Friend Finder Team</h6>
+    </div>
+
+  )
+}
+
+export default Footer;
